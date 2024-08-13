962251dcbf477107a49379c81e8f5a8a7296b1af
Athanasius206 <atanasiosroger2007@gmail.com>
2024-08-13 21:38:59 +0300

    Update ask.js

--- script/ask.js
+++ script/ask.js
@@ -1,8 +1,8 @@
 const fonts = {
-  a: "a", b: "b", c: "c", d: "d", e: "e", f: "f", g: "g", h: "h",
-  i: "i", j: "j", k: "k", l: "l", m: "m", n: "n", o: "o", 
-  p: "p", q: "q", r: "r", s: "s", t: "t", u: "u", v: "v", 
-  w: "w", x: "x", y: "y", z: "z" 
+  a: "A", b: "B", c: "C", d: "D", e: "E", f: "F", g: "G", h: "H",
+  i: "I", j: "J", k: "K", l: "L", m: "M", n: "N", o: "O", 
+  p: "P", q: "Q", r: "R", s: "D", t: "T", u: "U", v: "V", 
+  w: "W", x: "X", y: "Y", z: "Z" 
 };
 
 const axios = require('axios');
